6f57ff5ff8371738629a149a888c785ad3399f26
Anton Kokorin <ant.kkrn@gmail.com>
2023-03-30 21:42:41 +0300

    change pemissions

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -5,25 +5,21 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == "admin"
 
+    def has_object_permission(self, request, view, obj):
+        return request.user.role == "admin"
+
 
 class IsUser(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == "user"
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
 
 
 class IsModerator(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == "moderator"
 
-
-class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
-
-
-class IsAnonim(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
-
-    def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS
+        return request.user.role == "moderator"
